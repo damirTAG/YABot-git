2e543bbbaad9cc05c33c161586aa6075d90d52c5
damirTAG <damirtagilbayev17@gmail.com>
2023-08-28 01:56:12 +0600

    some fixes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 from config import TOKEN
+
+# TEXT FILES IMPORT BEGIN
+from allText import start_txt, text_txt, error_txt, help_txt
+# TEXT FILES IMPORT END
+
 import os
 import logging
 from random import randrange
@@ -10,14 +15,6 @@ from aiogram import Dispatcher, Bot, executor, types
 from aiogram import asyncio
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ChatActions
-from PIL import Image, ImageFilter, ImageFont, ImageDraw
-# from aiogram.dispatcher.filters import Text
-# from savify import Savify
-# from savify.types import Type, Format, Quality
-# from savify.utils import PathHolder
-# from requests import get
-# from json import loads
-# from bs4 import BeautifulSoup as bs
 # =========
 storage = MemoryStorage()
 bot = Bot(TOKEN, parse_mode=types.ParseMode.HTML)
@@ -30,23 +27,35 @@ logger = logging.getLogger()
 
 @dp.message_handler(commands='start')
 async def hello(message: types.Message):
-    start = "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Hello there! This is YouTube/TikTok/Reels video and audio bot installer\n\n🇰🇿 Сәлем! Бұл YouTube/TikTok/Reels/Twitch Clips/SoundcloudTracks бот орнатушысы"
+    start = start_txt
     await message.reply(text=start)
 
 
+@dp.message_handler(commands='help', commands_prefix='!/')
+async def help(message: types.Message):
+    help = help_txt
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(
+        InlineKeyboardButton(
+            text='Dev 🧑‍💻', url='https://github.com/damirTAG'),
+    )
+    await message.reply(text=help, reply_markup=keyboard)
+
 # close button
+
+
 @dp.callback_query_handler(text="close")
 async def close(call: types.CallbackQuery):
     await bot.delete_message(call.message.chat.id, call.message.message_id)
 
 
 # roll
-@dp.message_handler(commands=['roll', 'ролл'], commands_prefix='!/')
+@dp.message_handler(commands=['roll', 'ролл'], commands_prefix='!/.')
 async def rate(message: types.Message):
     nick = message.from_user.first_name
     chat_id = message.chat.id
     random = (randrange(101))
-    roll = f'🎱 <b>{nick}</b> роллит! (1-100): <code>{random}</code>!'
+    roll = f'🎱 <b>{nick}</b> роллит! [1-100]. Выпадает: <b>{random}</b>!'
     return await bot.send_message(chat_id=chat_id, reply_to_message_id=message.message_id, text=roll)
 
 # speech rec
@@ -55,6 +64,7 @@ async def rate(message: types.Message):
 @dp.message_handler(content_types=['voice', 'video_note'])
 async def get_audio_messages(message: types.Message):
     try:
+        await bot.send_chat_action(message.chat.id, ChatActions.TYPING)
         file_id = message.voice.file_id if message.content_type in [
             'voice'] else message.video_note.file_id
         file_info = await bot.get_file(file_id)
@@ -70,15 +80,11 @@ async def get_audio_messages(message: types.Message):
         os.remove(file_name)
         message_text = f"<i><b>{converter.audio_to_text()}</b></i>"
         del converter
-        await bot.send_chat_action(message.chat.id, ChatActions.TYPING)
-        await asyncio.sleep(3)
-        await bot.send_message(message.chat.id, message_text,
+        await bot.send_message(message.chat.id, text=message_text,
                                reply_to_message_id=message.message_id)
     except:
-        nowords = "<i>Хуй со рта убери</i>"
-        await bot.send_chat_action(message.chat.id, ChatActions.TYPING)
-        await asyncio.sleep(3)
-        await bot.send_message(message.chat.id, nowords,
+        nowords = "<i>Речь не распознана</i>"
+        await bot.send_message(message.chat.id, text=nowords,
                                reply_to_message_id=message.message_id)
 
 # video download
@@ -90,69 +96,100 @@ async def inline_keyboard_mp4(call: types.CallbackQuery):
     loading = "<i>Жүктеу | Loading</i>"
     await call.message.edit_text(text=loading)
     chat_id = call.message.chat.id
-    service = "twitch.tv"
-    if link.find(service) != -1:
-        print("downloading with 720p")
-        anotheroptions = {
-            'format': 'mp4[height<=720]',
-            'skip-download': True,
-            'noplaylist': True,
-            'outtmpl': 'video/%(title)s.%(ext)s',
-            'cookies-from-browser': 'chrome',
-        }
-        try:
-            with ytd.YoutubeDL(anotheroptions) as ytdl:
-                start = datetime.now()
-                ytdl.download([link])
-                end = datetime.now()
+    print("downloading with 1080p")
+    options = {'skip-download': True,
+               'format': 'mp4',
+               'outtmpl': 'video/%(title)s.%(ext)s',
+               'cookies-from-browser': 'chrome',
+               'cookies': 'cookies.txt',
+               'noplaylist': True,
+               }
+    try:
+        with ytd.YoutubeDL(options) as ytdl:
+            result = ytdl.extract_info("{}".format(link))
+            title = ytdl.prepare_filename(result)
+            start = datetime.now()
+            ytdl.download([link])
+            end = datetime.now()
+    except:
+        print(ValueError)
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(
+            InlineKeyboardButton(text='❌ Жабу | Close',
+                                 callback_data='close'),
+        )
+        await bot.delete_message(call.message.chat.id, call.message.message_id)
 
-        except:
-            keyboard = InlineKeyboardMarkup()
-            keyboard.add(
-                InlineKeyboardButton(text='❌ Жабу | Close',
-                                     callback_data='close'),
-            )
-            await bot.delete_message(call.message.chat.id, call.message.message_id)
-            error = f'<i>Жүктеу кезінде қате орын алды\nError while loading content</i>\n\nContact: @damirtag'
-            await bot.send_message(text=error, chat_id=chat_id, reply_to_message_id=message_id, reply_markup=keyboard)
-    else:
-        print("downloading with 1080p")
-        options = {'skip-download': True,
-                   'format': 'mp4',
-                   'outtmpl': 'video/%(id)s.%(ext)s',
-                   #    'cookies-from-browser': 'chrome',
-                   #    'cookies': 'cookies.txt',
-                   'noplaylist': True,
-                   }
-        try:
-            with ytd.YoutubeDL(options) as ytdl:
-                start = datetime.now()
-                ytdl.download([link])
-                end = datetime.now()
-        except ValueError:
-            keyboard = InlineKeyboardMarkup()
-            keyboard.add(
-                InlineKeyboardButton(text='❌ Жабу | Close',
-                                     callback_data='close'),
-            )
-            await bot.delete_message(call.message.chat.id, call.message.message_id)
-            error = f'<i>Жүктеу кезінде қате орын алды\nError while loading content</i>\n\nContact: @damirtag'
-            await bot.send_message(text=error, chat_id=chat_id, reply_to_message_id=message_id, reply_markup=keyboard)
-    result = ytdl.extract_info("{}".format(link))
-    title = ytdl.prepare_filename(result)
+        await bot.send_message(text=error_txt, chat_id=chat_id, reply_to_message_id=message_id, reply_markup=keyboard)
+
     video_title = result.get('title', None)
     video = open(f'{title}', 'rb')
     await bot.delete_message(call.message.chat.id, call.message.message_id)
-    await bot.send_chat_action(call.message.chat.id, ChatActions.UPLOAD_VIDEO)
-    await asyncio.sleep(3)
     loadtime = (end - start).total_seconds() * 1**1
     caption = f"Title: <a href='{link}'>{video_title}</a>\n<i>Loading time: {loadtime:.01f}sec</i>"
-    await bot.send_video(chat_id=chat_id, video=video, caption=caption, reply_to_message_id=message_id)
-    os.remove(title)
-    print("%s has been removed successfuly" % title)
+    try:
+        return await bot.send_video(chat_id=chat_id, video=video, caption=caption, reply_to_message_id=message_id)
+    except:
+        print(ValueError)
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(
+            InlineKeyboardButton(text='❌ Жабу | Close',
+                                 callback_data='close'),
+        )
+        await bot.delete_message(call.message.chat.id, call.message.message_id)
+        return await bot.send_message(text=error_txt, chat_id=chat_id, reply_to_message_id=message_id, reply_markup=keyboard)
+    finally:
+        # deleting file after
+        os.remove(title)
+        print("%s has been removed successfuly" % title)
+
+# if not youtube download
 
 
-# audio section here mp3/soundcloud/spotify
+@dp.message_handler(regexp='(?:https?://)?(?:www\.)?(?:vk\.com/clip|tiktok\.com|instagram\.com/reel/|instagram\.com/stories/|twitch\.tv/)')
+@dp.async_task
+async def loadvideo(message: types.Message):
+    await bot.send_chat_action(message.chat.id, ChatActions.RECORD_VIDEO)
+    options = {'skip-download': True,
+               'format': 'mp4',
+               'outtmpl': 'video/%(title)s.%(ext)s',
+               'cookies-from-browser': 'chrome',
+               'cookies': 'cookies.txt',
+               'noplaylist': True,
+               }
+    try:
+        with ytd.YoutubeDL(options) as ytdl:
+            print('downloading vk, tiktok, inst, twitch')
+            result = ytdl.extract_info("{}".format(link))
+            title = ytdl.prepare_filename(result)
+            ytdl.download([link])
+
+    except ValueError:
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(
+            InlineKeyboardButton(text='❌ Жабу | Close',
+                                 callback_data='close'),
+        )
+        await bot.send_message(text=error_txt, chat_id=message.chat.id, reply_to_message_id=message_id, reply_markup=keyboard)
+    video_title = result.get('title', None)
+    video = open(f'{title}', 'rb')
+    caption = f"<b>Title: <a href='{link}'>{video_title}</a></b>"
+    try:
+        return await bot.send_video(chat_id=message.chat.id, video=video, caption=caption, reply_to_message_id=message_id)
+    except ValueError:
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(
+            InlineKeyboardButton(text='❌ Жабу | Close',
+                                 callback_data='close'),
+        )
+        return await bot.send_message(text=error_txt, chat_id=message.chat.id, reply_to_message_id=message_id, reply_markup=keyboard)
+    finally:
+        # deleting file after
+        os.remove(title)
+        print("%s has been removed successfuly" % title)
+
+
+# audio section here mp3/soundcloud
 # audio download
 
 
@@ -181,14 +218,14 @@ async def inline_keyboard_mp3(call: types.CallbackQuery):
             ytdl.download([link])
             end = datetime.now()
     except:
+        print(ValueError)
         keyboard = InlineKeyboardMarkup()
         keyboard.add(
             InlineKeyboardButton(text='❌ Жабу | Close',
                                  callback_data='close'),
         )
         await bot.delete_message(call.message.chat.id, call.message.message_id)
-        error = f'<i>Жүктеу кезінде қате орын алды\nError while downloading content</i>\n\nContact: @damirtag'
-        await bot.send_message(text=error, chat_id=chat_id, reply_to_message_id=message_id, reply_markup=keyboard)
+        await bot.send_message(text=error_txt, chat_id=chat_id, reply_to_message_id=message_id, reply_markup=keyboard)
     result = ytdl.extract_info("{}".format(link))
     title = ytdl.prepare_filename(result)
     video_title = result.get('title', None)
@@ -217,7 +254,7 @@ async def soundload(message: types.Message):
         print("Unaware")
     else:
         print("======")
-    username = message.from_user.first_name
+    username = message.from_user.full_name
     msg = message.text
     print(f"[{current_date}] {username} : {msg}")
     print("downloading mp3 format | SOUNDCLOUD")
@@ -246,14 +283,14 @@ async def soundload(message: types.Message):
             ytdl.download([link])
             # end = datetime.now()
     except:
+        print(ValueError)
         keyboard = InlineKeyboardMarkup()
         keyboard.add(
             InlineKeyboardButton(text='❌ Жабу | Close',
                                  callback_data='close'),
         )
         chat_id = message.chat.id
-        error = f'<i>Жүктеу кезінде қате орын алды\nError while downloading content</i>\n\nContact: @damirtag'
-        await bot.send_message(text=error, chat_id=chat_id, reply_to_message_id=message_id, reply_markup=keyboard)
+        await bot.send_message(text=error_txt, chat_id=chat_id, reply_to_message_id=message_id, reply_markup=keyboard)
     result = ytdl.extract_info("{}".format(link))
     title = ytdl.prepare_filename(result)
     delete = (f'{title}.mp3')
@@ -262,11 +299,10 @@ async def soundload(message: types.Message):
     none = "Damir the best"
     video_title = result.get('title', none)
     uploader = result.get('uploader', none)
-    uploder_link = f'https://soundcloud.com/user-{uploader}'
-    like_count = result.get('like_count', none)
-    comment_count = result.get('comment_count', none)
+    uploader_id = result.get('uploader_id', none)
+    uploader_link = f'https://soundcloud.com/user-{uploader_id}'
     username = message.from_user.username
-    caption = f"<b><i>ℹ️ Info:</i>\n🎵 Track: <a href='{link}'>{video_title}</a>\n👤 Author: <a href='{uploder_link}'>{uploader}</a>\n❤️ Likes: <a href='{link}'>{like_count}</a>\n💬 Comments count: <a href='{link}'>{comment_count}</a></b>"
+    caption = f'<a href="{link}">{video_title}</a> — <a href="{uploader_link}">{uploader}</a>'
     chat_id = message.chat.id
     message_id = message.message_id
     try:
@@ -279,15 +315,14 @@ async def soundload(message: types.Message):
                                  callback_data='close'),
         )
         chat_id = message.chat.id
-        error = f'<i>Жүктеу кезінде қате орын алды\nError while sending content</i>\n\nContact: @damirtag'
-        return await bot.send_message(text=error, chat_id=chat_id, reply_to_message_id=message_id, reply_markup=keyboard)
+        return await bot.send_message(text=error_txt, chat_id=chat_id, reply_to_message_id=message_id, reply_markup=keyboard)
     finally:
         # deleting file after
         os.remove(delete)
         print("%s has been removed successfuly" % title)
 
 
-@dp.message_handler(regexp='(?:https?://)?(?:www\.)?(?:youtube\.com|youtu\.be|tiktok\.com|instagram\.com/reel/|pinterest.com|twitch\.tv/)')
+@dp.message_handler(regexp='(?:https?://)?(?:www\.)?(?:youtube\.com|youtu\.be)')
 @dp.async_task
 async def downloading(message: types.Message):
     current_date = datetime.now(pytz.timezone('Asia/Almaty'))
@@ -296,23 +331,22 @@ async def downloading(message: types.Message):
         print("Unaware")
     else:
         print("======")
-    username = message.from_user.first_name
+    username = message.from_user.full_name
     msg = message.text
     print(f"[{current_date}] {username} : {msg}")
     global link
     link = message.text
     global message_id
     message_id = message.message_id
-    global chat_id
-    chat_id = await message.chat.id
     # messages texts
-    text = "Қандай пішінде жүктеп алу керек?\nIn what type to download?"
+    text = text_txt
     keyboard = InlineKeyboardMarkup()
     keyboard.add(
         InlineKeyboardButton(text='📹', callback_data='download_mp4'),
         InlineKeyboardButton(text='🔊', callback_data='download_mp3'),
+        # InlineKeyboardButton(text='Image (TikTok)',
+        #                      callback_data='download_img'),
     )
-    await asyncio.sleep(0.1)
     return await message.reply(text=text, reply_markup=keyboard)
 
 if __name__ == '__main__':
